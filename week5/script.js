b52c8154f93dbcf1b7a8cdd53bb44affadb57091
joliecao865 <joliecao865@gmail.com>
2025-04-03 10:12:41 +1100

    jvs

---
+++ week5/script.js
@@ -0,0 +1,8 @@
+console.log("Hello");
+console.assert("how are you buddy?");
+
+// javascript, above is telling them to print those sentences
+// comments are to document and explain the logic of your code
+
+let myName = "Rohit";
+console.log("Hello", myName);
