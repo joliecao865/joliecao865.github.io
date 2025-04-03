4e6d4c0495429c36e7bc9e3bd36c2aeeba7c2905
joliecao865 <joliecao865@gmail.com>
2025-04-03 12:03:34 +1100

    shopping

--- week5/cart/script.js
+++ week5/cart/script.js
@@ -3,6 +3,7 @@ let shoppingCart = [
   { name: "Jeans", price: 50 },
   { name: "Sneakers", price: 80 },
   { name: "Backpack", price: 30 },
+  { name: "Shades", price: 100 },
 ];
 let total = 0;
 console.log(shoppingCart);
@@ -11,3 +12,9 @@ for (let i = 0; i < shoppingCart.length; i++) {
   console.log("Total so far is", total);
 }
 console.log("Final total is", total);
+
+let discount = 0.1;
+if (total > 100) {
+  let discountedPrice = total - total * discount;
+  console.log("Discounted price is", discountedPrice);
+}
