19d04c342d3733fe6596946800797ec3286ef04c
joliecao865 <joliecao865@gmail.com>
2025-04-03 11:38:52 +1100

    Update script.js

--- week5/weather/script.js
+++ week5/weather/script.js
@@ -2,4 +2,16 @@ function checkWeather() {
   let myTemp = document.querySelector("#myTemp");
   // google search for the element whose id is myTemp
   console.log(myTemp);
+  let temp = myTemp.value;
+  console.log("temp value is", temp);
+
+  if (temp < 10) {
+    console.log("it is freezing");
+  } else if (temp >= 10 && temp < 20) {
+    console.log("it is a pleasant weather");
+  } else if (temp >= 20 && temp < 35) {
+    console.log("it is nice and sunny");
+  } else if (temp >= 35) {
+    console.log("it is burning hot");
+  }
 }
