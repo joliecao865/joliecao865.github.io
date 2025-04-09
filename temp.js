0a4052c46e20495f5cfc00149a0304cbfe425ef4
joliecao865 <joliecao865@gmail.com>
2025-04-10 09:58:44 +1000

    temp

---
+++ temp.js
@@ -0,0 +1,17 @@
+// variables let and const
+// types of variables
+// numerical variable let a = 0 - + - * /
+// string/ text variabeles let name = "Rohit" 'rohit'
+// back ticks ` rohit
+//
+// `
+// boolean variables true false let isItRaining = false
+// objects
+// let myRecord = { name: "rk", id:1, isLocal: false };
+// myRecord.name = rk
+// arrays
+// let numbers = [2,4,6,8,10];
+// arrays start at 0 numbers[2] =.
+// if condition
+// for loops
+//  (javascript reads depending on line. if condition allows us to skip some lines (if false if true))
