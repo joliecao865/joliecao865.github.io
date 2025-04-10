2e7860d95c46e75587ca57a78c0a326c0f876e5d
joliecao865 <joliecao865@gmail.com>
2025-04-10 10:38:47 +1000

    dom

--- week 6/script.js
+++ week 6/script.js
@@ -1,44 +1,8 @@
-let a = 20;
-let b = 10;
-function add(val1, val2) {
-  let total = val1 + val2;
-  console.log(total);
-  return total;
-}
+const topHeading = document.querySelector("h1");
+console.log(topHeading);
 
-function substract(val1, val2) {
-  let res = val1 - val2;
-  //   console.log(res);
-  return res;
-}
+const myFirstPara = document.querySelector("p");
+console.log(myFirstPara);
 
-function whatIsMyGrade(marks) {
-  if (marks > 80) {
-    console.log("you got HD");
-  } else if (marks < 40) {
-    console.log("sorry you failed");
-  }
-}
-
-let score = 89;
-let msg = whatIsMyGrade(score);
-
-// let c = a + b;
-let c = add(a, b);
-// let c = add(10, 20);
-console.log(c);
-
-c = subtract(a);
-console.log(c);
-
-c = subtract(140, 56);
-// console.log(c);
-
-// c = 40 + 56;
-c = add(40, 56);
-console.log(c);
-
-a = 45;
-b = 6;
-c = a + b;
-console.log(c);
+const firstSubHeading = document.querySelector("#first-subheading");
+console.log(firstSubHeading);
