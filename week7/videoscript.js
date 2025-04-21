c8d07cc23e40a2527692bad994ef680525a5c4c1
joliecao865 <joliecao865@gmail.com>
2025-04-21 11:35:51 +1000

    starta2

--- week7/videoscript.js
+++ week7/videoscript.js
@@ -29,3 +29,30 @@ function pauseVideo() {
   myVideo.pause();
 }
 // -----------------------------------------
+
+// ----------------------------------------
+// my logic for playing sound
+// first I need to fetch the right button for play
+
+const playPauseButton = document.querySelector("#play-pause-button");
+console.log(playPauseButton);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPauseImg);
+// then I listen to clicks on that button
+playPauseButton.addEventListener("click", toggleVideo);
+
+// whenever someone clicks, the video is played
+function toggleVideo() {
+  if (myVideo.paused || myVideo.ended) {
+    // remember that pausED and endED
+    // either of that is true, it will happen
+    myVideo.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
+  } else {
+    myVideo.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+  }
+  //   myVideo.play();
+}
+// -----------------------------------------
